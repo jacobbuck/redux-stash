1e70e4453e579904ae5c0dbf22b3ccddd0fc9844
Jacob Buck <buck.jacob@gmail.com>
2018-09-23 11:48:44 +1200

    Add type checking to createStash and Storage

--- src/createStash.js
+++ src/createStash.js
@@ -1,8 +1,26 @@
-const createStash = ({ name, readOnly = false, selector, storage }) => ({
-  name,
-  readOnly,
-  selector,
-  storage,
-});
+import Storage from './storage/Storage';
+
+const createStash = ({ name, readOnly = false, selector, storage }) => {
+  if (process.env.NODE_ENV !== 'production') {
+    if (typeof name !== 'string') {
+      throw new TypeError('Expected "name" to be a string.');
+    }
+    if (typeof options !== 'boolean') {
+      throw new TypeError('Expected "readOnly" to be a boolean.');
+    }
+    if (typeof selector !== 'function') {
+      throw new TypeError('Expected "selector" to be a function.');
+    }
+    if (!(storage instanceof Storage)) {
+      throw new TypeError('Expected "storage" to be an instance of Storage.');
+    }
+  }
+  return {
+    name,
+    readOnly,
+    selector,
+    storage,
+  };
+};
 
 export default createStash;
