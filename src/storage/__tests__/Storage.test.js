98dd3b63446e13909476a7a97ba3defdb582faa7
Jacob Buck <buck.jacob@gmail.com>
2018-09-23 12:09:39 +1200

    Add tests for Storage

--- src/storage/__tests__/Storage.test.js
+++ src/storage/__tests__/Storage.test.js
@@ -1,11 +1,23 @@
 import Storage from '../Storage';
 
-test('stores "key" when constructed', () => {
+test('stores key when constructed', () => {
   const storage = new Storage('hi');
   expect(storage.key).toBe('hi');
 });
 
-test('stores "options" when constructed', () => {
+test('stores options when constructed', () => {
   const storage = new Storage('hi', { foo: 'bar' });
   expect(storage.options).toEqual({ foo: 'bar' });
+});
+
+test("throws an error when key isn't a string", () => {
+  expect(() => new Storage()).toThrow();
+  expect(() => new Storage(null)).toThrow();
+  expect(() => new Storage(true)).toThrow();
+  expect(() => new Storage({ foo: 'bar' })).toThrow();
+});
+
+test("throws an error when options isn't an object", () => {
+  expect(() => new Storage('hi', true)).toThrow();
+  expect(() => new Storage('hi', 'foo')).toThrow();
 });
