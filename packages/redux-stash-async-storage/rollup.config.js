ac96621e03b78dc8aa61a753185c1363ffef91f9
Jacob Buck <buck.jacob@gmail.com>
2020-03-08 03:21:32 +0000

    Build es module bundles

--- packages/redux-stash-async-storage/rollup.config.js
+++ packages/redux-stash-async-storage/rollup.config.js
@@ -2,10 +2,16 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/AsyncStorage.js',
-  output: {
-    file: 'lib/AsyncStorage.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'lib/AsyncStorage.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/AsyncStorage.esm.js',
+      format: 'esm',
+    }
+  ],
   external: ['redux-stash', 'react-native'],
   plugins: [babel()],
 };
--- packages/redux-stash-cookie-storage/rollup.config.js
+++ packages/redux-stash-cookie-storage/rollup.config.js
@@ -2,10 +2,16 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/CookieStorage.js',
-  output: {
-    file: 'lib/CookieStorage.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'lib/CookieStorage.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'lib/CookieStorage.esm.js',
+      format: 'esm',
+    }
+  ],
   external: ['js-cookie', 'redux-stash'],
   plugins: [babel()],
 };
