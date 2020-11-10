e7b88908798008e1a4b30310af277da3a8057be4
Jacob Buck <buck.jacob@gmail.com>
2020-11-10 19:57:57 +0000

    Generate source maps from rollup

--- packages/redux-stash-async-storage/rollup.config.js
+++ packages/redux-stash-async-storage/rollup.config.js
@@ -3,8 +3,8 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/AsyncStorage.js',
   output: [
-    { file: 'lib/AsyncStorage.cjs.js', format: 'cjs' },
-    { file: 'lib/AsyncStorage.esm.js', format: 'esm' },
+    { file: 'lib/AsyncStorage.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'lib/AsyncStorage.esm.js', format: 'esm', sourcemap: true },
   ],
   external: ['@react-native-community/async-storage', 'redux-stash'],
   plugins: [babel({ babelHelpers: 'bundled' })],
--- packages/redux-stash-cookie-storage/rollup.config.js
+++ packages/redux-stash-cookie-storage/rollup.config.js
@@ -3,8 +3,8 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/CookieStorage.js',
   output: [
-    { file: 'lib/CookieStorage.cjs.js', format: 'cjs' },
-    { file: 'lib/CookieStorage.esm.js', format: 'esm' },
+    { file: 'lib/CookieStorage.cjs.js', format: 'cjs', sourcemap: true },
+    { file: 'lib/CookieStorage.esm.js', format: 'esm', sourcemap: true },
   ],
   external: ['js-cookie', 'redux-stash'],
   plugins: [babel({ babelHelpers: 'bundled' })],
