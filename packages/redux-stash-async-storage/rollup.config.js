f1b993f6f9e01d7f401869821ace2775e7d29a6b
Jacob Buck <buck.jacob@gmail.com>
2020-11-11 07:48:38 +0000

    Set output.exports config in rollup to supress warning for redux-stash-async-storage and redux-stash-cookie-storage

--- packages/redux-stash-async-storage/rollup.config.js
+++ packages/redux-stash-async-storage/rollup.config.js
@@ -3,8 +3,17 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/AsyncStorage.js',
   output: [
-    { file: 'lib/AsyncStorage.cjs.js', format: 'cjs', sourcemap: true },
-    { file: 'lib/AsyncStorage.esm.js', format: 'esm', sourcemap: true },
+    {
+      exports: 'named',
+      file: 'lib/AsyncStorage.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'lib/AsyncStorage.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
   ],
   external: ['@react-native-community/async-storage', 'redux-stash'],
   plugins: [babel({ babelHelpers: 'bundled' })],
--- packages/redux-stash-cookie-storage/rollup.config.js
+++ packages/redux-stash-cookie-storage/rollup.config.js
@@ -3,7 +3,12 @@ import babel from '@rollup/plugin-babel';
 export default {
   input: 'src/CookieStorage.js',
   output: [
-    { file: 'lib/CookieStorage.cjs.js', format: 'cjs', sourcemap: true },
+    {
+      exports: 'named',
+      file: 'lib/CookieStorage.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
     { file: 'lib/CookieStorage.esm.js', format: 'esm', sourcemap: true },
   ],
   external: ['js-cookie', 'redux-stash'],
