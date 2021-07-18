45ffc85bf842f6a817d4c9e15aadba36d58daeb7
Jacob Buck <buck.jacob@gmail.com>
2021-07-18 21:47:54 +0000

    Minor changes to rehydrateStore test

--- packages/redux-stash/src/__tests__/rehydrateStore.test.js
+++ packages/redux-stash/src/__tests__/rehydrateStore.test.js
@@ -2,7 +2,8 @@ import { REQUEST_REHYDRATE } from '../actionTypes';
 import rehydrateStore from '../rehydrateStore';
 
 test('dispatches REQUEST_REHYDRATE action on store ', () => {
-  const store = { dispatch: jest.fn() };
+  const dispatch = jest.fn();
+  const store = { dispatch };
   rehydrateStore(store);
-  expect(store.dispatch).toHaveBeenCalledWith({ type: REQUEST_REHYDRATE });
+  expect(dispatch).toHaveBeenCalledWith({ type: REQUEST_REHYDRATE });
 });
