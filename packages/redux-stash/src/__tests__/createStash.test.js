b38f701c0f2ffbbf7307d8789ad43dd5124d5c5b
Jacob Buck <buck.jacob@gmail.com>
2021-07-18 21:47:02 +0000

    Refactor createStash tests to reduce duplication, check error messages and handle a couple more schenarios

--- packages/redux-stash/src/__tests__/createStash.test.js
+++ packages/redux-stash/src/__tests__/createStash.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import createStash from '../createStash';
 import LocalStorage from '../LocalStorage';
 
@@ -14,8 +17,8 @@ test('returns an object', () => {
   ).toEqual({
     name: 'foo',
     readOnly: false,
-    selector: expect.any(Function),
-    storage: expect.any(LocalStorage),
+    selector: defaultSelector,
+    storage: defaultStorage,
   });
   expect(
     createStash({
@@ -27,7 +30,7 @@ test('returns an object', () => {
     name: 'foo',
     readOnly: true,
     selector: expect.any(Function),
-    storage: expect.any(LocalStorage),
+    storage: defaultStorage,
   });
 });
 
@@ -39,125 +42,61 @@ test('ignores invalid properties', () => {
       storage: defaultStorage,
       foo: 'bar',
     })
-  ).not.toHaveProperty('foo', 'bar');
+  ).not.toHaveProperty('foo');
 });
 
-test("throws an error when name isn't a string", () => {
-  expect(() =>
-    createStash({
-      name: null,
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 420,
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: ['foo'],
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: { toString: () => 'foo' },
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
+test('throws an error when name isn’t a string', () => {
+  [null, 3840, ['foo'], new String('foo')].forEach((name) => {
+    expect(() =>
+      createStash({
+        name,
+        selector: defaultSelector,
+        storage: defaultStorage,
+      })
+    ).toThrow('Expected `name` to be a string');
+  });
 });
 
-test("throws an error when readOnly isn't a boolean", () => {
-  expect(() =>
-    createStash({
-      name: 'foo',
-      readOnly: null,
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      readOnly: 'true',
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      readOnly: [true],
-      selector: defaultSelector,
-      storage: defaultStorage,
-    })
-  ).toThrow();
+test('throws an error when readOnly isn’t a boolean', () => {
+  [null, 1, 'true', [true], new Boolean(true)].forEach((readOnly) => {
+    expect(() =>
+      createStash({
+        name: 'foo',
+        readOnly,
+        selector: defaultSelector,
+        storage: defaultStorage,
+      })
+    ).toThrow('Expected `readOnly` to be a boolean');
+  });
 });
 
-test("throws an error when selector isn't a function", () => {
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: false,
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: 'foo',
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: new (function Foo() {})(),
-      storage: defaultStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: { foo: 'bar' },
-      storage: defaultStorage,
-    })
-  ).toThrow();
+test('throws an error when selector isn’t a function', () => {
+  function Foo() {}
+  [false, 'foo', new Foo(), { call: jest.fn() }].forEach((selector) => {
+    expect(() =>
+      createStash({
+        name: 'foo',
+        selector: false,
+        storage: defaultStorage,
+      })
+    ).toThrow('Expected `selector` to be a function');
+  });
 });
 
-test("throws an error when storage isn't an instanceof Storage", () => {
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: defaultSelector,
-      storage: localStorage,
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: defaultSelector,
-      storage: function () {},
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: defaultSelector,
-      storage: new (function Foo() {})(),
-    })
-  ).toThrow();
-  expect(() =>
-    createStash({
-      name: 'foo',
-      selector: defaultSelector,
-      storage: { foo: 'bar' },
-    })
-  ).toThrow();
+test('throws an error when storage isn’t an instanceof Storage', () => {
+  class FooStorage {}
+  [
+    localStorage,
+    function () {},
+    new FooStorage(),
+    { getItem() {}, setItem() {}, deleteItem() {} },
+  ].forEach((storage) => {
+    expect(() =>
+      createStash({
+        name: 'foo',
+        selector: defaultSelector,
+        storage,
+      })
+    ).toThrow('Expected `storage` to be an instance of Storage');
+  });
 });
